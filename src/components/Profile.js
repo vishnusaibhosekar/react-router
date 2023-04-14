7c41bcb7080661dcc5b16ff75f5d522488da840d
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2023-04-14 16:14:34 -0400

    adds nestled routes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 
 export default function Profile () {
@@ -13,7 +13,7 @@ export default function Profile () {
     <main>
       <h1>{currentUser.username}</h1>
       <Link to={`edit`}>Edit</Link>
-      {/* Tell React Router where to render child routes` */}
+      <Outlet />
     </main>
   )
 }

9a3b2821f9fff15055546fc657840e2633f8b7a2
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2023-04-14 16:19:02 -0400

    adds logic to check if user is logged-in

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 
 export default function Profile () {
   const currentUser = useSelector(selectCurrentUser)
   const loggedIn = useSelector(selectIsLoggedIn);
-  
-  // use loggedIn to return a Navigate
+
+  if(!loggedIn) {
+    return <Navigate to="/sign-up" />
+  }
 
   return (
     <main>
